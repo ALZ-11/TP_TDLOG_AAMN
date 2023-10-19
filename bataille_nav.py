f360c3e13e2124ae6e918ab1cd90d332e84429ee
Adam Lozi <adamlozibus@gmail.com>
2023-10-19 08:56:21 +0100

    added a NoAmmoError exception object

--- bataille_nav.py
+++ bataille_nav.py
@@ -1,3 +1,8 @@
+class NoAmmoError(Exception):
+    def __init__(self, message="No ammo left. Cannot fire."):
+        self.message = message
+        super().__init__(self.message)
+
 class Weapon:
     def __init__(self, ammunitions : int, range : int):
         self.ammunitions = ammunitions
