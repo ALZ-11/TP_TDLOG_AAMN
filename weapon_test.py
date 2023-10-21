8d5f64adb3e1d78725700b917e5a79daf969b1ba
Adam Lozi <adamlozibus@gmail.com>
2023-10-21 17:14:18 +0100

    added with & w/o ammo unittest cases for Weapon's fire_at method

--- weapon_test.py
+++ weapon_test.py
@@ -1,8 +1,15 @@
 import unittest
-from weapon import Weapon
+from weapon import Weapon, NoAmmoError
 
 class TestWeapon(unittest.TestCase):
     def test_initialization(self):
         weapon = Weapon(10, 50)
         self.assertEqual(weapon.ammunitions, 10)
         self.assertEqual(weapon.range, 50)
+    def test_fire_at_with_ammo(self):
+        weapon = Weapon(ammunitions=10, range=50)
+        self.assertIsNone(weapon.fire_at(1, 2, 3))
+    def test_fire_at_without_ammo(self):
+        weapon = Weapon(ammunitions=0, range=50)
+        with self.assertRaises(NoAmmoError):
+            weapon.fire_at(1, 2, 3)
