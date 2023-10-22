d7faaa07e614ccf6d0055c6688e4e9c41dfd703d
Adam Lozi <adamlozibus@gmail.com>
2023-10-22 16:22:37 +0100

    added tests for the L.M anti air class

--- weapon_test.py
+++ weapon_test.py
@@ -36,3 +36,26 @@ class TestLanceMissilesAntiSurface(unittest.TestCase):
             self.assertIsNone(weapon.fire_at(1, 2, 0)) # consommer toute l'ammo
         with self.assertRaises(NoAmmoError):
             weapon.fire_at(1, 2, 0)
+
+class TestLanceMissilesAntiAir(unittest.TestCase):
+    def test_initialization(self):
+        weapon = Lance_missiles_antisurface()
+        self.assertEqual(weapon.ammunitions, 40)
+        self.assertEqual(weapon.range, 20)
+
+    def test_fire_at_within_range(self):
+        weapon = Lance_missiles_antisurface()
+        self.assertIsNone(weapon.fire_at(1, 2, 0))
+        self.assertEqual(weapon.ammunitions, 39)  
+
+    def test_fire_at_out_of_range(self):
+        weapon = Lance_missiles_antisurface()
+        with self.assertRaises(OutOfRangeError):
+            weapon.fire_at(1, 2, 0)
+
+    def test_fire_at_without_ammo(self):
+        weapon = Lance_missiles_antisurface()
+        for _ in range(40):
+            self.assertIsNone(weapon.fire_at(1, 2, 1)) # consommer toute l'ammo
+        with self.assertRaises(NoAmmoError):
+            weapon.fire_at(1, 2, 1)
