e729a181783d01833d54328c9118f12cb729277f
Adam Lozi <adamlozibus@gmail.com>
2023-10-22 16:48:38 +0100

    added tests for lance tropilles

--- weapon_test.py
+++ weapon_test.py
@@ -1,5 +1,5 @@
 import unittest
-from weapon import Weapon, Lance_missiles_antisurface, OutOfRangeError, NoAmmoError
+from weapon import *
 
 class TestWeapon(unittest.TestCase):
     def test_initialization(self):
@@ -32,30 +32,53 @@ class TestLanceMissilesAntiSurface(unittest.TestCase):
 
     def test_fire_at_without_ammo(self):
         weapon = Lance_missiles_antisurface()
-        for _ in range(50):
+        for i in range(50):
             self.assertIsNone(weapon.fire_at(1, 2, 0)) # consommer toute l'ammo
         with self.assertRaises(NoAmmoError):
             weapon.fire_at(1, 2, 0)
 
 class TestLanceMissilesAntiAir(unittest.TestCase):
     def test_initialization(self):
-        weapon = Lance_missiles_antisurface()
+        weapon = Lance_missiles_antiair()
         self.assertEqual(weapon.ammunitions, 40)
         self.assertEqual(weapon.range, 20)
 
     def test_fire_at_within_range(self):
-        weapon = Lance_missiles_antisurface()
-        self.assertIsNone(weapon.fire_at(1, 2, 0))
+        weapon = Lance_missiles_antiair()
+        self.assertIsNone(weapon.fire_at(1, 2, 1))
         self.assertEqual(weapon.ammunitions, 39)  
 
     def test_fire_at_out_of_range(self):
-        weapon = Lance_missiles_antisurface()
+        weapon = Lance_missiles_antiair()
         with self.assertRaises(OutOfRangeError):
             weapon.fire_at(1, 2, 0)
 
     def test_fire_at_without_ammo(self):
-        weapon = Lance_missiles_antisurface()
-        for _ in range(40):
+        weapon = Lance_missiles_antiair()
+        for i in range(40):
             self.assertIsNone(weapon.fire_at(1, 2, 1)) # consommer toute l'ammo
         with self.assertRaises(NoAmmoError):
             weapon.fire_at(1, 2, 1)
+            
+class TestLanceTropilles(unittest.TestCase):
+    def test_initialization(self):
+        weapon = Lance_tropilles()
+        self.assertEqual(weapon.ammunitions, 24)
+        self.assertEqual(weapon.range, 40)
+
+    def test_fire_at_within_range(self):
+        weapon = Lance_tropilles()
+        self.assertIsNone(weapon.fire_at(1, 2, 0))
+        self.assertEqual(weapon.ammunitions, 23)  
+
+    def test_fire_at_out_of_range(self):
+        weapon = Lance_tropilles()
+        with self.assertRaises(OutOfRangeError):
+            weapon.fire_at(1, 2, 1)
+
+    def test_fire_at_without_ammo(self):
+        weapon = Lance_tropilles()
+        for i in range(24):
+            self.assertIsNone(weapon.fire_at(1, 2, 0)) # consommer toute l'ammo
+        with self.assertRaises(NoAmmoError):
+            weapon.fire_at(1, 2, 0)
