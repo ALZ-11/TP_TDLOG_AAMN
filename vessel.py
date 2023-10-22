ed730a06aa8ef1487d94ac65a2b33fa9542a2b39
Adam Lozi <adamlozibus@gmail.com>
2023-10-22 22:37:24 +0100

    added basic methods in Vessel class

--- vessel.py
+++ vessel.py
@@ -5,3 +5,9 @@ class Vessel:
 		self.coordinates = coordinates
 		self.max_hits = max_hits
 		self.weapon = weapon
+	def get_coordinates(self):
+		return self.coordinates
+	def go_to(self, x, y, z):
+		self.coordinates = (x, y, z)
+	def fire_at(self, x, y, z):
+		self.weapon.fire_at(x, y, z)
