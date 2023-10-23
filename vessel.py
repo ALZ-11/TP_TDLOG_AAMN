2921c4a842c31a4599ce0cccc2dcf6ff4265a38f
Adam Lozi <adamlozibus@gmail.com>
2023-10-23 15:36:13 +0100

    added Cruiser class (Vessel subclass) with initialization

--- vessel.py
+++ vessel.py
@@ -1,4 +1,9 @@
-from weapon import Weapon
+from weapon import *
+
+class DestroyedError(Exception):
+    def __init__(self, message="The vessel is destroyed. Cannot get coordinates."):
+        self.message = message
+        super().__init__(self.message)
 
 class Vessel:
 	def __init__(self, coordinates : tuple, max_hits : int, weapon : Weapon):
@@ -6,8 +11,16 @@ class Vessel:
 		self.max_hits = max_hits
 		self.weapon = weapon
 	def get_coordinates(self):
+		try:
+			assert self.max_hits > 0
+		except:
+			raise DestroyedError
 		return self.coordinates
 	def go_to(self, x, y, z):
 		self.coordinates = (x, y, z)
 	def fire_at(self, x, y, z):
 		self.weapon.fire_at(x, y, z)
+
+class Cruiser(Vessel):
+	def __init__(self, coordinates):
+		super().__init__(coordinates, 6, Lance_missiles_antiair())
