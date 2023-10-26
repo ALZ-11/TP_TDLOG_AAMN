5e64798ed42c0889b1f529533beb60558a41850c
Adam Lozi <adamlozibus@gmail.com>
2023-10-26 02:20:03 +0100

    created a TestVessel class with all test cases

--- vessel_test.py
+++ vessel_test.py
@@ -10,20 +10,40 @@ class TestVessel(unittest.TestCase):
         self.assertEqual(vessel.coordinates, coordinates)
         self.assertEqual(vessel.max_hits, max_hits)
         self.assertEqual(vessel.weapon, weapon)
-    def test_get_coordinates(self):
+    def test_get_coordinates_not_destroyed(self):
         coordinates = (1, 2, 3)
         vessel = Vessel(coordinates, 5, Weapon(10, 50))
         self.assertEqual(vessel.get_coordinates(), coordinates)
-    def test_go_to(self):
+    def test_get_coordinates_is_destroyed(self):
+        coordinates = (1, 2, 3)
+        vessel = Vessel(coordinates, 0, Weapon(10, 50))
+        with self.assertRaises(DestroyedError):
+            vessel.get_coordinates()
+    def test_go_to_not_destroyed(self):
         initial_coordinates = (1, 2, 3)
         new_coordinates = (4, 5, 6)
         vessel = Vessel(initial_coordinates, 5, Weapon(10, 50))
         vessel.go_to(new_coordinates)
         self.assertEqual(vessel.coordinates, new_coordinates)
-        
-class TestCruiser(unittest.TestCase):
-    def test_initialization(self):
-        cruiser = Cruiser((1, 2, 3))
-        self.assertEqual(cruiser.coordinates, (1, 2, 3))
-        self.assertEqual(cruiser.max_hits, 6)
-        self.assertIsInstance(cruiser.weapon, Lance_missiles_antiair)
+    def test_go_to_is_destroyed(self):
+        initial_coordinates = (1, 2, 3)
+        new_coordinates = (4, 5, 6)
+        vessel = Vessel(initial_coordinates, 0, Weapon(10, 50))
+        with self.assertRaises(DestroyedError):
+            vessel.go_to(new_coordinates)
+    def test_fire_at_not_destroyed_within_range(self):
+        weapon = Weapon(10, 50)
+        vessel = Vessel((1, 2, 3), 5, weapon)
+        self.assertIsNone(vessel.fire_at(2, 3, 0))
+        self.assertEqual(weapon.ammunitions, 9)
+    def test_fire_at_is_destroyed(self):
+        weapon = Weapon(10, 50)
+        vessel = Vessel((1, 2, 3), 0, weapon)
+        with self.assertRaises(DestroyedError):
+            vessel.fire_at(1, 1, 1)
+    def test_fire_at_out_of_range(self):
+        weapon = Weapon(10, 1)
+        vessel = Vessel((0, 0, 0), 5, weapon)
+        with self.assertRaises(OutOfRangeError):
+            vessel.fire_at(1, 1, 1)
+        self.assertEqual(weapon.ammunitions, 9)
